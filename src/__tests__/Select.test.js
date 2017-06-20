b3c33331d0c1e9967bf2fc8b749a6a8c15ae0c86
Hunter Stevens <onebree@gmail.com>
2017-06-20 16:38:55 -0400

    WIP - more tests, focusing on props

--- src/__tests__/Select.test.js
+++ src/__tests__/Select.test.js
@@ -3,20 +3,20 @@ jest.dontMock('../components/Select');
 import React, { TestUtils } from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Select from '../components/Select';
-// import renderer from 'react-test-renderer';
 
 // TODO
-// data-include-blank
-// options => placeholder, selectOnTab, highlight
+// options => placeholder, selectOnTab, highlight, options
 // onChange
 // name
 // style
+// className
 
 // TODO
 // componentDidMount
 // componentWillUnmount
+// onChange
 
-describe('<Select />', () => {
+describe('Select', () => {
   const employees = [
     <option value={1} key={1}>Hunter Stevens</option>,
     <option value={2} key={2}>Jordan Byron</option>,
@@ -25,41 +25,70 @@ describe('<Select />', () => {
 
   let wrapper;
 
-  // beforeEach(() => {
-  //   wrapper = shallow(
-  //     <Select
-  //       value=""
-  //       onChange={(e) => console.log(e.target.value, e.target.innerText)}
-  //       className="test"
-  //       name="workers"
-  //       style={{ width: 300 }}
-  //     />
-  //   );
-  // });
+  let props;
+  let mountedSelect;
 
-  it('contains a select.testing element', () => {
-    wrapper = shallow(<Select className="testing" />)
+  const selectComponent = () => {
+    if (!mountedSelect) {
+      mountedSelect = mount(<Select {...props} />);
+    }
+    return mountedSelect;
+  };
 
-    expect(wrapper.find('select.testing')).toBeDefined;
+  // Resets the props and mountedSelect before every test
+  beforeEach(() => {
+    wrapper = undefined;
+    props = {};
+    mountedSelect = undefined;
+  });
+
+  it('always renders a select', () => {
+    // Method 1 - mounting component
+    const selects = selectComponent().find('select');
+    expect(selects.length).toEqual(1);
+
+    // Method 2 - using a shallow component
+    // wrapper = shallow(<Select />);
+    // expect(wrapper.find('select')).toBeDefined;
+  });
+
+  it('passes props down to select tag', () => {
+    // Method 1
+    props = { className: 'testing', name: 'test' };
+
+    const select = selectComponent().find('select');
+
+    // Includes `children` property
+    expect(Object.keys(select.props()).length).toBe(3);
+
+    // Method 2
+    // props = { className: 'testing', name: 'test', children: undefined };
+    //
+    // wrapper = shallow(<Select {...props} />);
+    //
+    // const select = wrapper.find('select').node;
+    //
+    // expect(select.props).toEqual(props);
+  });
+
+
+  describe('rendered select', () => {
+    xit('receives all props passed to Select', () => {
+    });
   });
 
   it('contains no options when not specified', () => {
-    wrapper = shallow(<Select />)
+    wrapper = shallow(<Select />);
 
     expect(wrapper.find('option')).toNotBeDefined;
   });
 
-  xit('contains two options when passed as children', () => {
+
+  it('contains two options when passed as children', () => {
     wrapper = mount(<Select>{employees}</Select>);
+    const { children } = wrapper.node.props;
 
-    expect(wrapper).toHaveProperty('component.props.props.children');
-
-    // expect(wrapper.props.children).toEqual(
-    //   expect.arrayContaining(employees)
-    // );
-  });
-
-  xit('', () => {
+    expect(children.length).toEqual(3);
+    expect(children).toEqual(expect.arrayContaining(employees));
   });
 });
-
