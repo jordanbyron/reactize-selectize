1d6da38c6bfb8e1047a77850e20fd7b3595bc94e
Hunter Stevens <onebree@gmail.com>
2017-06-23 15:10:02 -0400

    Finish testing props.children

--- src/__tests__/Select.test.js
+++ src/__tests__/Select.test.js
@@ -17,9 +17,6 @@ import Select from '../components/Select';
 // onChange
 
 describe('Select', () => {
-
-  let wrapper;
-
   let props;
   let mountedSelect;
 
@@ -32,19 +29,12 @@ describe('Select', () => {
 
   // Resets the props and mountedSelect before every test
   beforeEach(() => {
-    wrapper = undefined;
     props = {};
     mountedSelect = undefined;
   });
 
   it('always renders a select', () => {
-    // Method 1 - mounting component
-    const selects = selectComponent().find('select');
-    expect(selects.length).toEqual(1);
-
-    // Method 2 - using a shallow component
-    // wrapper = shallow(<Select />);
-    // expect(wrapper.find('select')).toBeDefined;
+    expect(selectComponent().find('select')).toHaveLength(1);
   });
 
   it('passes props down to select tag', () => {
@@ -54,30 +44,20 @@ describe('Select', () => {
     const select = selectComponent().find('select');
 
     // Includes `children` property
-    expect(Object.keys(select.props()).length).toBe(3);
-
-    // Method 2
-    // props = { className: 'testing', name: 'test', children: undefined };
-    //
-    // wrapper = shallow(<Select {...props} />);
-    //
-    // const select = wrapper.find('select').node;
-    //
-    // expect(select.props).toEqual(props);
+    expect(Object.keys(select.props())).toHaveLength(3);
   });
 
-  it('contains no options when not specified', () => {
-    wrapper = shallow(<Select />);
 
-    expect(wrapper.find('option')).toNotBeDefined;
-  });
+  describe('when `children` is not defined', () => {
+    it('does not render option elements', () => {
+      const { children } = selectComponent().node.props;
 
-  describe('rendered select', () => {
-    xit('receives all props passed to Select', () => {
+      expect(children).toBeUndefined();
+      expect(selectComponent().find('option')).toHaveLength(0);
     });
   });
 
-  describe('children', () => {
+  describe('when `children` is defined', () => {
     it('renders 3 options', () => {
       const animals = [
         <option value="cat" key={1}>Cat</option>,
@@ -85,17 +65,18 @@ describe('Select', () => {
         <option value="goat" key={3}>Goat</option>
       ];
 
-      wrapper = mount(<Select>{animals}</Select>);
-      const { children } = wrapper.node.props;
+      props.children = animals;
 
-      expect(wrapper.find('option')).toHaveLength(3);
-      expect(children.length).toEqual(3);
+      const { children } = selectComponent().node.props;
+
+      expect(children).toHaveLength(3);
       expect(children).toEqual(expect.arrayContaining(animals));
+      expect(selectComponent().find('option')).toHaveLength(3);
     });
 
-    xit('renders 2 optgroups with 3 options each', () => {
+    it('renders 2 optgroups with 3 options each', () => {
       const mammals = (
-        <optgroup label="Mammals">
+        <optgroup key={1} label="Mammals">
           <option value="cat" key={1}>Cat</option>
           <option value="dog" key={2}>Dog</option>
           <option value="goat" key={3}>Goat</option>
@@ -103,26 +84,89 @@ describe('Select', () => {
       );
 
       const reptiles = (
-        <optgroup label="Reptiles">
+        <optgroup key={2} label="Reptiles">
           <option value="turtle" key={4}>Turtle</option>
           <option value="lizard" key={5}>Lizard</option>
           <option value="snake" key={6}>Snake</option>
         </optgroup>
       );
 
-      // create wrapper, or mount component?
+      props.children = [mammals, reptiles];
+
+      const { children } = selectComponent().node.props;
+
+      expect(children).toHaveLength(2);
+      expect(children).toEqual(expect.arrayContaining([mammals, reptiles]));
+      expect(selectComponent().find('optgroup')).toHaveLength(2);
+      expect(selectComponent().find('option')).toHaveLength(6);
+    });
+
+  });
+
+  describe('when `options.options` is undefined', () => {
+    it('does not render option elements', () => {
+      const { options } = selectComponent().props();
+
+      expect(options).toBeUndefined();
+      expect(selectComponent().find('option')).toHaveLength(0);
     });
   });
 
+  describe('when `options.options` is defined', () => {
+    // FIXME
+    // `option` and `optgroup` tags are not found when passing
+    // them as Selectize options
+
+    it('renders 3 options', () => {
+      const animals = [
+        { value: 'dog', key: 1, name: 'Dog' },
+        { value: 'cat', key: 2, name: 'Cat' },
+        { value: 'duck', key: 3, name: 'Duck' },
+      ];
+
+      props.options = { options: animals };
+
+      const { options } = selectComponent().node.props.options;
+
+      expect(options).toHaveLength(3);
+      expect(options).toEqual(expect.arrayContaining(animals));
+      // expect(selectComponent().find('option')).toHaveLength(3);
+    });
+
+    it('renders 3 optgroups with 2 options each', () => {
+      const animals = {
+        options: [
+          { class: 'mammal', value: 'dog', key: 1, name: 'Dog' },
+          { class: 'mammal', value: 'cat', key: 2, name: 'Cat' },
+          { class: 'bird', value: 'duck', key: 3, name: 'Duck' },
+          { class: 'bird', value: 'chicken', key: 4, name: 'Chicken' },
+          { class: 'reptile', value: 'snake', key: 5, name: 'Snake' },
+          { class: 'reptile', value: 'lizard', key: 6, name: 'Lizard' }
+        ],
+        optgroups: [
+          { value: 'mammal', key: 1, label: 'Mammal' },
+          { value: 'bird', key: 2, label: 'Bird' },
+          { value: 'reptile', key: 3, label: 'Reptile' }
+        ],
+        optgroupField: 'class',
+        labelField: 'name'
+      };
+
+      props.options = animals;
+
+      const { options, optgroups } = selectComponent().node.props.options;
+
+      expect(options).toHaveLength(6);
+      expect(options).toEqual(expect.arrayContaining(animals.options));
+      // expect(selectComponent().find('option')).toHaveLength(6);
+      expect(optgroups).toHaveLength(3);
+      expect(optgroups).toEqual(expect.arrayContaining(animals.optgroups));
+      // expect(selectComponent().find('optgroup')).toHaveLength(3);
+    });
+  });
 
   describe('Selectize API options', () => {
     xit('renders a placeholder', () => {
-    });
-
-    xit('renders 3 options', () => {
-    });
-
-    xit('renders 2 optgroups with 3 options each', () => {
     });
   });
 });
